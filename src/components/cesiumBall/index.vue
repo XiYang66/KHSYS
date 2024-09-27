703f5e8ea40ad45c92490775428f3fc65015f3c2
fox <1842208351@qq.com>
2024-09-27 10:44:10 +0800

    ''

--- src/components/cesiumBall/index.vue
+++ src/components/cesiumBall/index.vue
@@ -18,17 +18,18 @@ const CesiumStoreInit = CesiumStore()
 // import { addImageryProvider } from '@/utils/cesium/layers/imagery.js'
 import DatGui from '@/components/datGui/index.vue'
 
-// 生命周期
 onMounted(async () => {
     let viewer = await init({
         container: 'cesiumContainer',
         timeline: true,
     });
     await CesiumStoreInit.SET_VIEWER(viewer);
-    // setupTimeline(viewer)
-    // loadSatelliteCzml(viewer)
+    loadCzml(viewer, 'models/simpleCZML.czml')
+
+})
+const loadCzml = (viewer, url) => {
     // 加载 CZML 数据并设置 clock
-    Cesium.CzmlDataSource.load(simpleCZML).then((czmlDataSource) => {
+    Cesium.CzmlDataSource.load(url).then((czmlDataSource) => {
         // 将数据源添加到 Viewer
         viewer.dataSources.add(czmlDataSource);
 
@@ -46,97 +47,8 @@ onMounted(async () => {
         // 设置时间轴的显示范围
         viewer.timeline.zoomTo(czmlClock.startTime, czmlClock.stopTime);
     });
-})
+}
 
-
-
-
-// 加载卫星 CZML 数据，并让卫星在轨道上移动
-const start = ref(null);
-const stop = ref(null);
-let simpleCZML = '/models/simpleCZML.czml'
-// const loadSatelliteCzml = (viewer) => {
-//     viewer.shouldAnimate = true
-//     const satelliteNames = ["Satellite/xpg2.0"];
-//     const satelliteAll = [];
-//     viewer.dataSources.removeAll(); // 清除所有现有数据源
-//     viewer.dataSources
-//         .add(Cesium.CzmlDataSource.load(simpleCZML))
-//         .then((dataSource) => {
-//             for (let i = 0; i < satelliteNames.length; i++) {
-//                 satelliteAll.push(dataSource.entities.getById(satelliteNames[i]));
-//             }
-//             for (let i = 0; i < satelliteAll.length; i++) {
-//                 // 更新卫星位置
-//                 const updatePosition = () => {
-//                     const positions = satelliteAll[i].position.getValue(
-//                         viewer.clock.currentTime
-//                     );
-//                     if (positions) {
-//                         const cartographic =
-//                             viewer.scene.globe.ellipsoid.cartesianToCartographic(
-//                                 positions
-//                             );
-//                         const lat = Cesium.Math.toDegrees(cartographic.latitude);
-//                         const lng = Cesium.Math.toDegrees(cartographic.longitude);
-//                         const hei = parseFloat(cartographic.height / 2.1);
-//                         return Cesium.Cartesian3.fromDegrees(lng, lat, hei);
-//                     }
-//                 };
-
-//                 // 更新卫星高度
-//                 const updateHeight = () => {
-//                     const positions = satelliteAll[i].position.getValue(
-//                         viewer.clock.currentTime
-//                     );
-//                     if (positions) {
-//                         const cartographic =
-//                             viewer.scene.globe.ellipsoid.cartesianToCartographic(
-//                                 positions
-//                             );
-//                         return parseFloat(cartographic.height);
-//                     }
-//                 };
-
-//                 // 添加实体，显示卫星的可视化模型
-//                 viewer.entities.add({
-//                     id: "Satellitegreen",
-//                     position: new Cesium.CallbackProperty(updatePosition, false),
-//                     cylinder: {
-//                         HeightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-//                         length: new Cesium.CallbackProperty(updateHeight, false),
-//                         topRadius: 0.0,
-//                         bottomRadius:
-//                             new Cesium.CallbackProperty(updateHeight, false).getValue() /
-//                             6,
-//                         material: Cesium.Color.GREEN.withAlpha(0.8),
-//                         outline: true,
-//                         numberOfVerticalLines: 0,
-//                         outlineColor: Cesium.Color.GREEN.withAlpha(0.8),
-//                     },
-//                 });
-//             }
-//         });
-// };
-// const setupTimeline = (viewer) => {
-//     start.value = Cesium.JulianDate.addHours(
-//         Cesium.JulianDate.fromDate(new Date()),
-//         8, // 东八区时间
-//         new Cesium.JulianDate()
-//     );
-//     stop.value = Cesium.JulianDate.addSeconds(
-//         start.value,
-//         1000, // 持续时间 1000 秒
-//         new Cesium.JulianDate()
-//     );
-
-//     viewer.clock.startTime = start.value.clone();
-//     viewer.clock.stopTime = stop.value.clone();
-//     viewer.clock.currentTime = start.value.clone();
-//     viewer.clock.clockRange = Cesium.ClockRange.LOOP_STOP; // 停止后不循环
-//     viewer.clock.multiplier = 2; // 时间倍速
-//     // viewer.timeline.zoomTo(start.value, stop.value); // 缩放时间线范围
-// };
 
 
 
